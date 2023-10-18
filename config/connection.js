7207c925e4a92d3bdd857674d9b6fb7ca72b2b2a
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-18 15:22:41 -0400

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// require('dotenv').config({path: path.join(__dirname, '../.env')});
+const path = require('path');
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
